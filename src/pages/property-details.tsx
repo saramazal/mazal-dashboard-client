c9add49fe7240f68e7a9a93e18672df7ec315e35
Sara Mazal <mazalwebdesign@gmail.com>
2023-03-05 13:36:54 +0200

    Property card id

--- src/pages/property-details.tsx
+++ src/pages/property-details.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import {
+  Box,
+  Stack,
+  Typography,
+} from "@pankod/refine-mui";
 
-function propertyDetails() {
+import { useDelete, useGetIdentity, useShow } from '@pankod/refine-core';
+import { useParams, useNavigate } from '@pankod/refine-react-router-v6';
+import { ChatBubble, Delete, Edit, Phone, Place, Star } from '@mui/icons-material';
+
+import { CustomButton } from 'components';
+
+const PropertyDetails = () => {
+  const navigate = useNavigate();
+  const { data: user } = useGetIdentity();
+  const { queryResult } = useShow();
+  const { mutate } = useDelete();
+  const { id } = useParams();
+
+  const { data, isLoading, isError } = queryResult;
+
+  const propertyDetails = data?.data ?? {};
+
+  if (isLoading) {
+      return <div>Loading...</div>;
+  }
+
+  if (isError) {
+      return <div>Something went wrong!</div>;
+  }
+
+
   return (
-    <div>propertyDetails</div>
+    <Box>
+      <Stack>
+        <Typography>propertyDetails
+        </Typography>
+      </Stack></Box>
   )
 }
 
-export default propertyDetails
+export default PropertyDetails;
